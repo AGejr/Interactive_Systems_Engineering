2f36f23d61b5d41f67e91f354225c1c55d9462e3
albni20 <albni20@student.sdu.dk>
2023-11-16 18:55:48 +0100

    updated app

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
 # Endpoint for the front page
 @app.route('/')
 def front_page():
-    return render_template('frontpage.html')
+    # Retrieve trending songs and albums from the database (sample data)
+    trending_songs = get_trending_songs()
+    trending_albums = get_trending_albums()
+
+    return render_template('frontpage.html', songs=trending_songs, albums=trending_albums)
+
+# Function to retrieve trending songs from the database
+def get_trending_songs():
+    # Connect to the MySQL database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password123",
+        database="MusicDB"
+    )
+    cursor = mydb.cursor()
+
+    # Execute a query to fetch trending songs (replace with your actual query)
+    cursor.execute("SELECT title FROM Song LIMIT 4")  # Fetching 4 trending songs as an example
+    trending_songs = [song[0] for song in cursor.fetchall()]
+
+    cursor.close()
+    mydb.close()
+
+    return trending_songs
+
+# Function to retrieve trending albums from the database
+def get_trending_albums():
+    # Connect to the MySQL database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="password123",
+        database="MusicDB"
+    )
+    cursor = mydb.cursor()
+
+    # Execute a query to fetch trending albums (replace with your actual query)
+    cursor.execute("SELECT title FROM Album LIMIT 4")  # Fetching 4 trending albums as an example
+    trending_albums = [album[0] for album in cursor.fetchall()]
+
+    cursor.close()
+    mydb.close()
+
+    return trending_albums
 
 # Endpoint for login
 @app.route('/login')
@@ -19,6 +64,7 @@ def search():
         query = request.args.get('query')
         # Perform search logic here based on the 'query' parameter
         return f"Search results for: {query}"  # Replace this with your search logic
+
 
 if __name__ == '__main__':
     app.run(debug=True)
