1dd907f2305e61eb73f67a44d9391dc06bf1aac2
albni20 <albni20@student.sdu.dk>
2023-12-04 13:43:49 +0100

    fixed bad imports

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
-import redirect
-import url_for
 
 app = Flask(__name__)
 
